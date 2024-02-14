43630a7df03bbd286b831cc35074280152fadc87
alialauren1 <157066441+alialauren1@users.noreply.github.com>
2024-02-14 15:50:46 -0800

    working encoder code
    now just need to do motor

--- encoder_reader.py
+++ encoder_reader.py
@@ -35,50 +35,56 @@ class Encoder:
         
         """   
         #initialize encoder pins
-        self.enc_name = enc_name
         self.pin1 = pyb.Pin(pin1, pyb.Pin.OUT_PP)
         self.pin2 = pyb.Pin(pin2, pyb.Pin.OUT_PP)
         
         #initialize timer channels
-        AR = 0xFFFF
-        self.tim = pyb.Timer(timer, prescaler=0, period=AR) # period is AR, if just period w/out PS -> actual period in [s]
+        self.AR = int(0xFFFF)
+        self.tim = pyb.Timer(timer, prescaler=0, period=self.AR) # period is AR, if just period w/out PS -> actual period in [s]
         self.ch1 = self.tim.channel(1, pyb.Timer.ENC_AB, pin=pin1) 
         self.ch2 = self.tim.channel(2, pyb.Timer.ENC_AB, pin=pin2)
         
-        prev_enc_count = 0 # initially zero because no previous count
+        self.previous_counter_val = 0 # init previous counter value, starts at zero
+        self.delta_summed = 0
+        
+        print('init-ed')
         
     def read(self):
         """
          Reads the current count of the encoder.
          
-         When it returns;
-         
-         int: The current count of the encoder
-        """        
-        self.counter_val = self.tim.counter() # counter_val
-        counter_val = self.counter_val # stored
+         Returns:
+         INSRT WORDS
+        """
+        # In the meantime
+        # This code below returns both encoder vals
+        self.current_counter_val = self.tim.counter()
+        self.delta = self.current_counter_val - self.previous_counter_val
         
-        # Alia's code here
-        delta = counter_val - prev_enc_count
- 
-        # calculate delta in count -> RETURN previous counter val
-        # RETURN TUPLE store delta val
+        print('current counter val', self.current_counter_val)
+        print('delta val', self.delta)
         
-        # if delta > (AR+1)/2 :
-            # delta -= (AR+1) # underflow
-        
-        # if delta < (AR+1)/2 :
-            # delta += (AR+1) # overflow
+        if self.delta >= (self.AR+1)/2:
+            self.delta -= self.AR # underflow
             
-        # add up delta vals -> RETURN total elapsed encoder pos += delta val
-
-        # Previous code:
-        self.counter_val = self.tim.counter() # counter_val
-        self.counter_val_prev = self.counter_val
-        #print('counter_val before yield',self.counter_val_prev)
-        #yield self.counter_val # gives us current position
-        #print('counter_val after yield',self.counter_val)
+        elif self.delta <= -(self.AR+1)/2:
+            self.delta += self.AR # overflow
+        else:
+            pass
         
+        self.delta_summed += self.delta # keeps summing the deltas
+        
+        print('delta total',self.delta_summed)
+        
+        # total count
+        #self.total_count = self.previous_counter_val + self.current_counter_val
+        #print('total count',self.total_count)
+        
+        print('prev counter val',self.previous_counter_val)
+        # save previous value
+        self.previous_counter_val = self.current_counter_val
+        
+
     def zero(self):
         """
          Resets the encoder back to zero.
@@ -86,8 +92,7 @@ class Encoder:
          When it returns;
          
          int: The new count after resetting back to zero.
-        """
-        prev_enc_count = counter_val # stores previous count 
+        """       
         self.tim.counter(0)
         
 
@@ -96,46 +101,20 @@ if __name__ == "__main__":
     enc1 = Encoder("enc1", pyb.Pin.board.PB6, pyb.Pin.board.PB7, 4)
     enc2 = Encoder("enc2", pyb.Pin.board.PC6, pyb.Pin.board.PC7, 8)
     
-    state = 0
+    #M_Timer = pyb.Timer(3, frequency=1000) # 1000 Hz
     
-    S0_INIT = 0
-    S1_RUN = 1
-    S2_COUNT_RESET = 2
     
     # continues to read encoder values for testing until "Ctrl-C" is pressed
     while True:
         try:
-            if (state == S0_INIT):
-                print('Init State')
-                state = S1_RUN
-            #iterate class
-            #next(read_enc1)
-            elif (state == S1_RUN):
-                print('Run State')
-#                 read_enc1 = enc1.read()
-#                 read_enc2 = enc2.read()
-                read_enc1, read_enc2 = enc1.read(),  enc2.read() # return tuple
-                encoder_reading = (read_enc1, read_enc2)
-                state = S2_COUNT_RESET
-                
-           elif (state == S2_COUNT_RESET): # will reset encoder to zero every data collection
-               #reset_enc1 = enc1.zero()
-               #reset_enc2 = enc2.zero()
-               reset_enc1, reset_enc2 = enc1.zero(), enc2.zero()
-            
-           else:
-               pass
-            
-            #make space between iterations
-            print("")
-            
-            time.sleep(0.5)
-        
+            #enc1.read()
+            #print('------')
+            enc2.read()
+            print('------------------')
+            time.sleep(1)
+         
         # Trying to catch the "Ctrl-C" keystroke to break out
         # of the program cleanly
         except KeyboardInterrupt:
             break
         
-        
-        
-    D
